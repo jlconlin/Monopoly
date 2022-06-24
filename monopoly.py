e4390de65ad604fe4b2ba08db2dbafe3fdd8a06e
Jeremy Lloyd Conlin <jlconlin@me.com>
2022-06-24 08:11:28 -0600

    Adding arguments.

--- monopoly.py
+++ monopoly.py
@@ -1,3 +1,5 @@
+import argparse
+import pathlib
 
 from monopoly.neighborhood import Neighborhood
 
@@ -6,10 +8,20 @@ if __name__ == "__main__":
 
     print("Monopoly")
 
-    with open("metadata/properties.json") as pJSON:
+    parser = argparse.ArgumentParser(description='Play monopoly')
+    parser.add_argument('--properties', type=pathlib.Path,
+                        default="metadata/properties.json",
+                        help="JSON description of the properties")
+    parser.add_argument('--neighborhoods', type=pathlib.Path,
+                        default="metadata/neighborhoods.json",
+                        help="JSON description of the neighborhoods")
+
+    args = parser.parse_args()
+
+    with args.properties.open('r') as pJSON:
         properties = json.load(pJSON)["properties"]
 
     neighborhoods = {}
-    with open("metadata/neighborhoods.json") as nJSON:
+    with args.neighborhoods.open('r') as nJSON:
         for name, hood in  json.load(nJSON)['neighborhoods'].items():
             neighborhoods[name] = Neighborhood(**hood, allProperties=properties)
